3166f40f92c25efaa8edac4ed18218f629be3c7f
Muhibbul hasan <muhibbulhasan6@gmail.com>
2024-02-06 23:02:57 +0600

    add

---
+++ dist/db.js
@@ -0,0 +1,41 @@
+const products = [
+    {
+        id: "123abcgjtty",
+        name: "Muhib",
+        image: "m.jpg",
+        price: 29.32,
+        quantity: 30,
+        onStock: true,
+        category: "321avc",
+    },
+    {
+        id: "123abcfff",
+        name: "Muhib",
+        image: "m.jpg",
+        price: 29.32,
+        quantity: 30,
+        onStock: true,
+        category: "321avc",
+    },
+    {
+        id: "123abcf",
+        name: "Muhib",
+        image: "m.jpg",
+        price: 29.32,
+        quantity: 30,
+        onStock: true,
+        category: "321avc",
+    },
+    {
+        id: "123abcg",
+        name: "Muhib",
+        image: "m.jpg",
+        price: 29.32,
+        quantity: 30,
+        onStock: true,
+        category: "321avc",
+    },
+];
+export const db = {
+    products,
+};
---
+++ dist/index.js
@@ -0,0 +1,44 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { db } from "./db.js";
+const typeDefs = `#graphql
+  type Product {
+    id: ID!
+name: String
+image:String
+price:Float
+quantity:Int
+onStock:Boolean
+category:ID
+   
+    
+  }
+ 
+ 
+  type Query {
+    products: [Product]
+  }
+`;
+const books = [
+    {
+        title: "The Awakening",
+        author: "Kate Chopin",
+    },
+    {
+        title: "City of Glass",
+        author: "Paul Auster",
+    },
+];
+const resolvers = {
+    Query: {
+        products: () => db.products,
+    },
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
